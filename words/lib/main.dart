d7da3a93bce78979781b45f5aa00636fcf4bd66b
qdongatuw <127705040+qdongatuw@users.noreply.github.com>
2023-08-29 01:41:49 -0500

    Update main.dart

--- words/lib/main.dart
+++ words/lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
+
 class DragAndDropApp extends StatefulWidget {
   @override
   _DragAndDropAppState createState() => _DragAndDropAppState();
@@ -21,61 +22,73 @@ class DragAndDropApp extends StatefulWidget {
 class _DragAndDropAppState extends State<DragAndDropApp> {
   List<String> draggableItems = ['Item 1', 'Item 2', 'Item 3'];
   List<String> droppedItems = [];
+  List<Widget> rowWidgt = [];
+
+  Container _newEntry(int length, bool editable) {
+    return Container(
+      width: length > 1? 200:50,
+      height: 80,
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
+      child: TextField(
+            enabled: editable,
+            maxLength: length,
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              //hintText: '',
+            )),) ;
+  }
+
+  Container _shadow(int length, bool editable){
+    return Container(
+      width: length > 1? 200:50,
+      height: 80,
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), border: Border.all(style: BorderStyle.solid)),
+      child: const Center(child: Text('')),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Drag and Drop App'),
+        title: Text('Words'),
       ),
       body: Column(
         children: [
           SizedBox(height: 20),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: draggableItems.map((item) {
-              return Draggable<String>(
-                data: item,
-                child: Container(
-                  width: 80,
-                  height: 80,
-                  color: Colors.blue,
-                  child: Center(child: Text(item, style: TextStyle(color: Colors.white))),
-                ),
-                feedback: Material(
-                  child: Container(
-                    width: 80,
-                    height: 80,
-                    color: Colors.blue.withOpacity(0.7),
-                    child: Center(child: Text(item, style: TextStyle(color: Colors.white))),
-                  ),
-                ),
-              );
-            }).toList(),
+            children: [
+              Draggable<Widget>(data: _newEntry(1, true), feedback: _shadow(1, false), child: _newEntry(1, false)),
+              Draggable<Widget>(data: _newEntry(10, true), feedback: _shadow(10, false), child: _newEntry(10, false)),
+              
+            ],
           ),
           SizedBox(height: 20),
-          DragTarget<String>(
+          DragTarget<Widget>(
             builder: (context, candidateData, rejectedData) {
               return Container(
-                width: 200,
+                // width: 200,
                 height: 100,
                 color: Colors.grey,
-                child: Center(
-                  child: Text(
-                    droppedItems.isNotEmpty ? droppedItems.last : 'Drop Here',
-                    style: TextStyle(color: Colors.white),
-                  ),
+                child: Row(
+                  children: rowWidgt,
                 ),
               );
             },
             onAccept: (item) {
               setState(() {
-                droppedItems.add(item);
+                // droppedItems.add(item);
+                //item.
+                rowWidgt.add(item);
               });
             },
           ),
           SizedBox(height: 20),
           Text('Dropped Items: ${droppedItems.join(", ")}'),
+          ElevatedButton(onPressed: (){setState(() {
+            rowWidgt.clear();
+          });}, child: const Text('Clear'))
         ],
       ),
     );
